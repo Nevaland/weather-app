d7e6e04ac92065503601ea1ba05c0a9707207739
Nevaland <deanh0716@gmail.com>
2021-04-04 08:08:51 +0900

    Add sample weather with icon

--- Weather.js
+++ Weather.js
@@ -1,21 +1,52 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import PropTypes from "prop-types";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-export default function Weather({ temp }) {
+export default function Weather({ condition, temp }) {
   return (
     <View style={styles.container}>
-      <Text>{temp}</Text>
+      <View style={styles.halfContainer}>
+        <MaterialCommunityIcons
+          name="weather-lightning-rainy"
+          size={96}
+          color="black"
+        />
+        <Text style={styles.temp}>{temp}℃</Text>
+      </View>
+      <View style={styles.halfContainer}>
+        <Text>Something Text</Text>
+      </View>
     </View>
   );
 }
 
 Weather.propTypes = {
   temp: PropTypes.number.isRequired,
+  condition: PropTypes.oneOf([
+    "Thunderstorm",
+    "Drizzle",
+    "Rain",
+    "Snow",
+    "Atmosphere",
+    "Clear",
+    "Coulds",
+    "Haze",
+    "Mist",
+    "Dust",
+  ]).isRequired,
 };
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  temp: {
+    fontSize: 42,
+  },
+  halfContainer: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
