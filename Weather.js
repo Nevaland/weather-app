fa26ccf87c0f0b0b3a8cd9046512a17dcaf24db2
Nevaland <deanh0716@gmail.com>
2021-04-04 08:46:34 +0900

    Implement linear gradient background

--- Weather.js
+++ Weather.js
@@ -1,23 +1,82 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { StatusBar } from "expo-status-bar";
 import PropTypes from "prop-types";
+import { LinearGradient } from "expo-linear-gradient";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
+const weatherOptions = {
+  Thunderstorm: {
+    iconName: "weather-lightning",
+    gradient: ["#373B44", "#4286f4"],
+  },
+  Drizzle: {
+    iconName: "weather-hail",
+    gradient: ["#89F7FE", "#66A6FF"],
+  },
+  Rain: {
+    iconName: "weather-rainy",
+    gradient: ["#00C6FB", "#005BEA"],
+  },
+  Snow: {
+    iconName: "weather-snowy",
+    gradient: ["#7DE2FC", "#B9B6E5"],
+  },
+  Atmosphere: {
+    iconName: "weather-hail",
+    gradient: ["#89F7FE", "#66A6FF"],
+  },
+  Clear: {
+    iconName: "weather-sunny",
+    gradient: ["#FF7300", "#FEF253"],
+  },
+  Clouds: {
+    iconName: "weather-cloudy",
+    gradient: ["#D7D2CC", "#304352"],
+  },
+  Mist: {
+    iconName: "weather-hail",
+    gradient: ["#4DA0B0", "#D39D38"],
+  },
+  Dust: {
+    iconName: "weather-hail",
+    gradient: ["#4DA0B0", "#D39D38"],
+  },
+  Haze: {
+    iconName: "weather-hail",
+    gradient: ["#4DA0B0", "#D39D38"],
+    title: "Haze",
+    subtitle: "Just don't go outside.",
+  },
+};
 export default function Weather({ condition, temp }) {
   return (
-    <View style={styles.container}>
+    <LinearGradient
+      // Background Linear Gradient
+      colors={
+        !!weatherOptions[condition]
+          ? weatherOptions[condition].gradient
+          : [("#4c669f", "#3b5998")]
+      }
+      style={styles.container}
+    >
+      <StatusBar style="auto" />
       <View style={styles.halfContainer}>
         <MaterialCommunityIcons
-          name="weather-lightning-rainy"
+          name={
+            !!weatherOptions[condition]
+              ? weatherOptions[condition].iconName
+              : "weather-sunset"
+          }
           size={96}
-          color="black"
+          color="white"
         />
         <Text style={styles.temp}>{temp}℃</Text>
       </View>
       <View style={styles.halfContainer}>
         <Text>Something Text</Text>
       </View>
-    </View>
+    </LinearGradient>
   );
 }
 
@@ -45,6 +104,7 @@ const styles = StyleSheet.create({
   },
   temp: {
     fontSize: 42,
+    color: "white",
   },
   halfContainer: {
     flex: 1,
